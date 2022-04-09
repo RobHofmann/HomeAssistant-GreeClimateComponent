297b3162d6e41f5bb490c070ec955f57530ef30b
Rob Hofmann <rob.hofmann@outlook.com>
2022-04-09 16:00:44 +0200

    Fix for 2022.4
    Fix for 2022.4 - removed import from old fans module

--- custom_components/gree/climate.py
+++ custom_components/gree/climate.py
@@ -26,8 +26,6 @@ from homeassistant.const import (
     STATE_ON, STATE_OFF, STATE_UNKNOWN, 
     TEMP_CELSIUS, PRECISION_WHOLE, PRECISION_TENTHS)
 
-from homeassistant.components.fan import SPEED_HIGH, SPEED_LOW, SPEED_MEDIUM
-
 from homeassistant.helpers.event import (async_track_state_change)
 from homeassistant.core import callback
 from homeassistant.helpers.restore_state import RestoreEntity
@@ -67,7 +65,7 @@ MAX_TEMP = 30
 # fixed values in gree mode lists
 HVAC_MODES = [HVAC_MODE_AUTO, HVAC_MODE_COOL, HVAC_MODE_DRY, HVAC_MODE_FAN_ONLY, HVAC_MODE_HEAT, HVAC_MODE_OFF]
 
-FAN_MODES = ['Auto', SPEED_LOW, 'Medium-Low', SPEED_MEDIUM, 'Medium-High', SPEED_HIGH, 'Turbo', 'Quiet']
+FAN_MODES = ['Auto', 'Low', 'Medium-Low', 'Medium', 'Medium-High', 'High, 'Turbo', 'Quiet']
 SWING_MODES = ['Default', 'Swing in full range', 'Fixed in the upmost position', 'Fixed in the middle-up position', 'Fixed in the middle position', 'Fixed in the middle-low position', 'Fixed in the lowest position', 'Swing in the downmost region', 'Swing in the middle-low region', 'Swing in the middle region', 'Swing in the middle-up region', 'Swing in the upmost region']
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
